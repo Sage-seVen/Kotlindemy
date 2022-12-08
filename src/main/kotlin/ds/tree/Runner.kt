abf52b1320beed9412e3adc43450a133ac6c62a1
Sahil Sagar <sahil.sagar@udaan.com>
2022-12-09 01:49:04 +0530

    Adding Tree

---
+++ src/main/kotlin/ds/tree/Runner.kt
@@ -0,0 +1,44 @@
+package ds.tree
+
+fun makeBikeTree(): TreeNode<String>{
+
+    val tree = TreeNode("Bikes")
+
+    val sportBikes = TreeNode("Sports")
+    val nakedBikes = TreeNode("Naked")
+
+
+    val parallelTwin = TreeNode("ParallelTwin")
+    val v4 = TreeNode("V4")
+    val inline4 = TreeNode("Inline4")
+
+    val cruiser = TreeNode("Cruiser")
+    val roadster = TreeNode("Roadster")
+    val bobber = TreeNode("Bobber")
+
+
+    tree.add(sportBikes)
+    tree.add(nakedBikes)
+
+    sportBikes.add(parallelTwin)
+    sportBikes.add(v4)
+    sportBikes.add(inline4)
+
+    nakedBikes.add(cruiser)
+    nakedBikes.add(roadster)
+    nakedBikes.add(bobber)
+
+    return tree
+}
+
+fun main(){
+    val tree = makeBikeTree()
+
+    //Depth-First
+    tree.forEachDepthFirst { println(it.value) }
+
+    println("*************************")
+
+    //Bread-first
+    tree.forEachLevelOrder { println(it.value) }
+}
--- src/main/kotlin/ds/tree/TreeNode.kt
+++ src/main/kotlin/ds/tree/TreeNode.kt
@@ -1,5 +1,41 @@
 package ds.tree
 
-class TreeNode {
+import ds.queue.QueueImpl
+
+typealias Visitor<T> = (TreeNode<T>) -> Unit
+
+class TreeNode<T>(val value : T) {
+    private val children: MutableList<TreeNode<T>> = mutableListOf()
+
+    fun add(child: TreeNode<T>){
+        children.add(child)
+    }
+
+    fun forEachDepthFirst(visit: Visitor<T>){
+        visit(this)
+
+        children.forEach{
+            it.forEachDepthFirst { visit(it) }
+        }
+    }
+
+    fun forEachLevelOrder(visit: Visitor<T>){
+        visit(this)
+
+        val queue = QueueImpl<TreeNode<T>>()
+        children.forEach{
+            queue.enqueue(it)
+        }
+
+        var node = queue.dequeue()
+        while (node != null){
+            visit(node)
+
+            node.children.forEach {
+                queue.enqueue(it)
+            }
+            node = queue.dequeue()
+        }
+    }
 
 }
